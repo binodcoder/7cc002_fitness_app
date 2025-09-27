6be88b34035380ce00e4cfb2ed8db7c7c409ac02
Binod Bhandari <binodcoder@gmail.com>
2025-09-27 13:01:19 +0100

    chore(sync data layer): have data source use SyncModel type and avoid domain entities in data layer signatures

--- lib/features/appointment/data/datasources/sync_remote_data_source.dart
+++ lib/features/appointment/data/datasources/sync_remote_data_source.dart
@@ -1,10 +1,9 @@
 import 'package:http/http.dart' as http;
 import 'package:fitness_app/core/errors/exceptions.dart';
-import 'package:fitness_app/features/appointment/domain/entities/sync.dart';
 import 'package:fitness_app/features/appointment/data/models/sync_data_model.dart';
 
 abstract class SyncRemoteDataSource {
-  Future<SyncEntity> sync(String email);
+  Future<SyncModel> sync(String email);
 }
 
 class SyncRemoteDataSourceImpl implements SyncRemoteDataSource {
@@ -12,11 +11,11 @@ class SyncRemoteDataSourceImpl implements SyncRemoteDataSource {
 
   SyncRemoteDataSourceImpl({required this.client});
 
-  Future<SyncEntity> _sync(String url) async {
+  Future<SyncModel> _sync(String url) async {
     final response = await client
         .get(Uri.parse(url), headers: {'Content-Type': 'application/json'});
     if (response.statusCode == 200) {
-      // Parse into model which extends domain entity
+      // Parse into model which extends the domain entity
       return syncModelFromJson(response.body);
     } else {
       throw ServerException();
@@ -24,6 +23,6 @@ class SyncRemoteDataSourceImpl implements SyncRemoteDataSource {
   }
 
   @override
-  Future<SyncEntity> sync(String email) =>
+  Future<SyncModel> sync(String email) =>
       _sync("https://wlv-c4790072fbf0.herokuapp.com/api/v1/sync/email/$email");
 }
